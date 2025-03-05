1b92a4045efa28b46bb43bbe39d265e5ceab5043
Juan-Alejandro-Arriaga <juanalejandroarriaganava@gmail.com>
2025-03-05 14:08:41 -0600

    Agregar regla para requerir salto de línea al final de cada archivo en ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,14 @@ export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
+
+  //Regla de salto de línea al final de cada archivo
+  {
+    rules: {
+      'eol-last': ['error', 'always'] 
+    }
+  }
 ];
+
+
+
