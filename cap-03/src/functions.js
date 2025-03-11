7c18ec5f0f827014c96ae9e4ce20bbf7b0b9e140
Juan-Alejandro-Arriaga <juanalejandroarriaganava@gmail.com>
2025-03-11 14:49:13 -0600

    Resumen del capitulo 3 de JS

---
+++ cap-03/src/functions.js
@@ -0,0 +1,25 @@
+//Cosas del capitulo 3
+let billion = 1e9; 
+console.log("Se imprime 1 billón: "+billion)
+
+console.log("Suma de 2 números: "+(1 + 2)+ " los numeros base son 1 + 2");
+
+
+//MATH object
+console.log("Valor de PI: "+Math.PI);
+
+console.log("2 elevado a la 10 es: "+Math.pow(2, 5));
+
+console.log("Texto en 2 lineas\n\tsegunda linea con tabulación");
+
+let mensaje = "Hola "+"mundo";
+console.log(mensaje);
+
+
+//Ciclo for
+
+for (let i = 0; i < 3; i++) {
+    console.log("Número: "+i);
+}
+
+
